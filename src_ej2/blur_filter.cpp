b8a23b47ca0ee785cccbbd707b26665bcd86aad9
earosselot <earosselot@gmail.com>
2019-11-22 16:30:37 -0300

    Ejercicio 2 puntos 1 y 2 en python (ej2.py) y el ejercicio3 en blur_filter.cpp
    los de python "funcionan"
    hay que chekear las imagenes de salida que no esta programado
    
    el blur en c++ debería funcionar, pero no lo probe

--- src_ej2/blur_filter.cpp
+++ src_ej2/blur_filter.cpp
@@ -2,4 +2,21 @@
 
 void blur_filter(float * im_res, float * im, int ii, int jj)
 {
+    /* for que recorre el "centro" de la matriz im y va completando im_res */
+    for (int i = 1, i < ii - 1, i++){
+        for (int j = 1, j < jj -1, j++){
+            im_res[((i * ii) + j)] = (im[((i * ii) + j + 1)] + im[((i * ii) + j - 1)] + im[(((i - 1) * ii) + j)] + im[(((i + 1) * ii) + j)]) / 4
+        }
+     }
+
+     /*este for recorre los bordes izquierdo y derecho y les asigna valor 0 */
+     for (int i = 0, i < ii, i++){
+        im_res[(i * ii)] = 0
+        im_res[(i * ii) + jj] = 0
+     }
+     /*este for recorre los bordes superior e inferior y les asigna valor 0 */
+     for (int j = 0, j < jj, j++){
+        im_res[j] = 0
+        im_res[(ii * (ii - 1)) + j] = 0
+     }
 }
--- src_ej2/ej2.py
+++ src_ej2/ej2.py
@@ -1,16 +1,41 @@
-import imfilters
-from scipy import misc
+# import imfilters
+# from scipy import misc
+# import scipy
+import imageio
 import numpy as np
 
-def gray_filter(a):
-    # COMPLETAR
-    pass
 
-def blur_filter(a):
-    # COMPLETAR
-    pass
+def gray_filter(img):
+    """filtro que hace una transformacion de una imagen color a una en escala de grises con ciertos valores"""
 
-# utilizar misc.imread(archivo, matriz) para leer imágenes de un archivo
-#          misc.imsave(archivo, matriz) para escribir imágenes a un archivo
+    # genero una matriz de 0 del tamaño de la imagen
+    x = img.shape[0]
+    y = img.shape[1]
+    res = np.zeros([x, y])
 
-# COMPLETAR EL RESTO DEL CÓDIGO PEDIDO A CONTINUACIÓN
+    # hago el filtrado
+    res = 0.3 * img[:, :, 0] + 0.6 * img[:, :, 1] + 0.11 * img[:, :, 2]
+    return res
+
+
+def blur_filter(img):
+    """Filtro que recibe una imagen matriz en grises y devuelve una matriz difuminada"""
+
+    # genero una matriz de 0 del tamaño de la imagen
+    filas = img.shape[0]
+    col = img.shape[1]
+    res = np.zeros([filas, col])
+
+    # los dos for anidados recorren toda la imagen excepto lo bordes
+    for i in range(1, filas - 1):
+        for j in range(1, col - 1):
+            # este calculo ganera el valor promedio que requiere el blur
+            res[i, j] = (img[i - 1, j] + img[i + 1, j] + img[i, j - 1] + img[i, j + 1]) / 4
+    return res
+
+
+
+im = imageio.imread('MaleLionNamibia.jpg')
+print(im.shape)
+res = gray_filter(im)
+print(res)

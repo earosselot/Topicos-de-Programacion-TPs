32fabd3a79f28c93bab01261259b42cc41b3dcb2
Eduardo <earosseot@gmail.com>
2019-11-25 16:02:29 -0300

    terminado hasta el mergeSort

--- src_ej1/Ejercicio1.py
+++ src_ej1/Ejercicio1.py
@@ -44,7 +44,8 @@ def distanciaMinima(l):
             distancias.append(distancia(l[i], l[j]))
     return min(distancias)
 
-def distanciaMinimaDyC(l,algoritmo):
+
+def distanciaMinimaDyC(l, algoritmo):
     """Esta función es análoga a 'distanciaMinima', pero utiliza la técnica de Divide & Conquer"""
     
     #Preprocesamiento: ordenamiento de la lista l
@@ -57,61 +58,92 @@ def distanciaMinimaDyC(l,algoritmo):
     else:
         print("Algoritmo inválido, usar 'up', 'python' ó 'merge'")
 
-
     
 #funciones para implementar upsort.
 #VER (aparentemente python comparar la coordenada x por default)
 
-def maxPos(a,b,c):
+
+def maxPos(a, b, c):
     """Busca la posición del elemento más grande de una lista entre los índices b y c"""
-    sublista = a[b:(c+1)] #(c+1) porque quiero incluir c
+
+    sublista = a[b:(c+1)]   #(c+1) porque quiero incluir c
     pos = 0
     maximo = sublista[0]
-    for i in range(len(sublista)):  #VER (uso fuerza bruta, no sé si está bien)
-        if maximo <= sublista[i]:
+    for i in range(1, len(sublista)):      #VER (uso fuerza bruta, no sé si está bien)
+        if maximo < sublista[i]:
             maximo = sublista[i]
             pos = i
     return pos
 
-def upSort(l): #es el código que Esteban dió en la teórica
+
+def upSort(l): # es el código que Esteban dió en la teórica
     """Ordena una lista l utilizando la técnica de upsort """
+
     actual = len(l) - 1
     m = 0
     while actual > 0:
-        m = maxPos(l,0,actual)
-        a[m],a[actual] = a[actual],a[m]
+        m = maxPos(l, 0, actual)
+        a[m], a[actual] = a[actual], a[m]
         actual -= 1
 
-#funciones para implementar merge
+
+#funciones para implementar merge sort
+
 def dividirEnDos(l):
-    """Divide a la lista l en dos mitades, no necesariamente iguales"""
-    if len(l) == 2 or len(l) == 1:
+    """Divide a la lista l en dos mitades, si la lista es mayor a 2.
+    Si tiene cantidad impar de elementos, la primer mitad es mas chica"""
+
+    if len(l) < 3:
         return l
     else:
         sublist1 = l[:int((len(l)/2))] 
         sublist2 = l[int((len(l)/2)):]
     return sublist1, sublist2
-        
+
+
+def merge(l1, l2):
+    """funcion que a partir de dos listas ordenadas (l1 y l2), devuelve una lista con los elementos
+    de l1 y l2 ordenados."""
+
+    # creo una lista vacia para guardar los elementos ordenados
+    merged = []
+
+    # ciclo que se ejecuta hasta que una de las listas no tiene elementos
+    while len(l1) > 0 and len(l2) > 0:
+        # estos if comparan los primeros elementos de la lista, guardan el menor en la lista ordenada,
+        # y lo eliminan de la lista original (para que el bucle termine)
+        if l1[0] <= l2[0]:
+            merged.append(l1[0])
+            l1.pop(0)
+        else:
+            merged.append(l2[0])
+            l2.pop(0)
+
+    # pone al final de la lista ordenada los elementos restantes de la lista que no se vació (y lo devuelve)
+    if len(l1) > 0:
+        return merged + l1
+    else:
+        return merged + l2
+
 
 def mergeSort(l):
     """Ordena una lista l utilizando la técnica de merge"""
+
     if len(l) == 1:
         return l
     elif len(l) == 2:
         if l[0] > l[1]:
-            l[0],l[1] = l[1],l[0]
+            l[0], l[1] = l[1], l[0]
         return l
     else:
-        listas = dividirEnDos(l)
-        print(mergeSort(listas[0]))
-        print(mergeSort(listas[1]))
-        
-        
+        lista_dividida = dividirEnDos(l)
+        return merge(mergeSort(lista_dividida[0]), mergeSort(lista_dividida[1]))
 
 
 
-a = listaDePuntos('datitos.txt')
-print("Lista de Coordenadas \n",a,"\n")
-print("Distancia minima \n", distanciaMinima(a),"\n")
-mergeSort(a)
+datos = listaDePuntos('datitos.txt')
+print("Lista de Coordenadas \n",datos,"\n")
+print("Distancia minima \n", distanciaMinima(datos), "\n")
+print('mergeSort: ', mergeSort(datos))
 #print(distancia(a[0], a[1]))
+print(datos)

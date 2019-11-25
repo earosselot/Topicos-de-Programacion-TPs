a788e42419b54ae4e527da3ea0129ef031458f9a
maaxi.f <maxi.perezfrasette@gmail.com>
2019-11-25 10:00:31 -0300

    Ejercicio 1 con nuevas funciones auxiliares (upsort completay mergesort incompleta)

--- src_ej1/Ejercicio1.py
+++ src_ej1/Ejercicio1.py
@@ -1,6 +1,6 @@
 # Topicos de Programacion 2019 - Trabajo Practico - Ejercicio 1 #
 # Grupo N ####
-# Perez Frastte, Maximiliano - Rosselot, Eduardo Agustin
+# Perez Frasette, Maximiliano - Rosselot, Eduardo Agustin
 
 def entero(a):
     """funcion que convierte los numeros (que estan como string) en una lista a float"""
@@ -44,8 +44,74 @@ def distanciaMinima(l):
             distancias.append(distancia(l[i], l[j]))
     return min(distancias)
 
+def distanciaMinimaDyC(l,algoritmo):
+    """Esta función es análoga a 'distanciaMinima', pero utiliza la técnica de Divide & Conquer"""
+    
+    #Preprocesamiento: ordenamiento de la lista l
+    if algoritmo == "python":
+        l.sort()
+    elif algoritmo == "up":
+        upSort(l)
+    elif algoritmo == "merge":
+        mergeSort(l)
+    else:
+        print("Algoritmo inválido, usar 'up', 'python' ó 'merge'")
+
+
+    
+#funciones para implementar upsort.
+#VER (aparentemente python comparar la coordenada x por default)
+
+def maxPos(a,b,c):
+    """Busca la posición del elemento más grande de una lista entre los índices b y c"""
+    sublista = a[b:(c+1)] #(c+1) porque quiero incluir c
+    pos = 0
+    maximo = sublista[0]
+    for i in range(len(sublista)):  #VER (uso fuerza bruta, no sé si está bien)
+        if maximo <= sublista[i]:
+            maximo = sublista[i]
+            pos = i
+    return pos
+
+def upSort(l): #es el código que Esteban dió en la teórica
+    """Ordena una lista l utilizando la técnica de upsort """
+    actual = len(l) - 1
+    m = 0
+    while actual > 0:
+        m = maxPos(l,0,actual)
+        a[m],a[actual] = a[actual],a[m]
+        actual -= 1
+
+#funciones para implementar merge
+def dividirEnDos(l):
+    """Divide a la lista l en dos mitades, no necesariamente iguales"""
+    if len(l) == 2 or len(l) == 1:
+        return l
+    else:
+        sublist1 = l[:int((len(l)/2))] 
+        sublist2 = l[int((len(l)/2)):]
+    return sublist1, sublist2
+        
+
+def mergeSort(l):
+    """Ordena una lista l utilizando la técnica de merge"""
+    if len(l) == 1:
+        return l
+    elif len(l) == 2:
+        if l[0] > l[1]:
+            l[0],l[1] = l[1],l[0]
+        return l
+    else:
+        listas = dividirEnDos(l)
+        print(mergeSort(listas[0]))
+        print(mergeSort(listas[1]))
+        
+        
+
+
 
 a = listaDePuntos('datitos.txt')
-print(a)
-print(distanciaMinima(a))
-print(distancia(a[0], a[1]))
+print("Lista de Coordenadas \n",a,"\n")
+print("Distancia minima \n", distanciaMinima(a),"\n")
+mergeSort(a)
+#print(distancia(a[0], a[1]))

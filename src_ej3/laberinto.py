ac160bf8a2799bd8f306ed25447040742760bd54
maaxi.f <maxi.perezfrasette@gmail.com>
2019-11-29 16:52:58 -0300

    Muy poco del ej3 (archivo de la clase Laberinto)

--- src_ej3/laberinto.py
+++ src_ej3/laberinto.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
 
 class Laberinto(object):
-	def __init__(self, parent=None):
-		self.parent = parent
+    """Vamos de definir funciones para crear un laberinto """
+	
+    #La función init se inicializa siempre que se llama a la clase Laberinto
+    def __init__(self, parent=None):
+        self.parent = parent
    
 	##### interfaz (metodos publicos)
-
+    def cargar(self,fn):
+        "Carga un archivo .txt que representa al laberinto"
+        self.fn = fn
+        with open (fn,"r") as entrada:
+            return entrada
+    
+    def tamano(self):
+        "Devuelve una tupla con la cantidad de filas y columnas"
+    
+    def resetear(self):
+        "limpia el laberinto"
+    
+    def getPosicionRata(self):
+        "devuelve una tupla con las coordenadas de la rata"
+        
+    
+    
 	####
 	#### COMPLETAR CON LOS METODOS PEDIDOS
 	####
 
 	##### auxiliares (metodos privados)
 
+"""
 	def _redibujar(self):
 	    if self.parent is not None:
 	        self.parent.update()
+"""
